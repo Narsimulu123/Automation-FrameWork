2bbba592ebc0dc5ef469be9f85a811ae804df7d4
HP <HP@Narsimulu>
2023-03-21 14:30:58 +0530

    Sample practice Testcase1 committed

---
+++ AutomationFramework/src/test/java/practice/SamplePractice.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class SamplePractice {
+	@Test
+	public void practice() {
+		System.out.println("Sample Practice Created");
+	}
+
+}
